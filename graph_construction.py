ef644b383ce9b7f9e055f570d6d19c875321dc75
Chris Hodapp <hodapp87@gmail.com>
2017-11-28 14:49:38 -0500

    Added stubs for some functions

--- graph_construction.py
+++ graph_construction.py
@@ -6,3 +6,19 @@
 # Date: 2017-11-28
 ###########################################################################
 
+def classifier(learning_rate, use_dropout):
+    """Builds (but does not execute) a TensorFlow graph for image
+    classification on this dataset.
+    
+    Arguments:
+    learning_rate - Learning rate to use
+    use_dropout - Boolean for whether this network should use dropout
+
+    Returns:
+    model -- Output of tf.global_variables_initializer()
+    train_op -- TensorFlow node for optimizing this network
+    accuracy -- Node giving the accuracy score on current batch
+    x -- Node for input placeholder variable
+    y -- Node for label placeholder variable
+    """
+    pass
--- learn.py
+++ learn.py
@@ -5,3 +5,7 @@
 # Author: Chris Hodapp (hodapp87@gmail.com)
 # Date: 2017-11-28
 ###########################################################################
+
+def train_model(model, num_epochs, batch_size, learning_rate):
+    pass
+
