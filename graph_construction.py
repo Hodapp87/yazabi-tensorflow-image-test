d5ca26a8e876f4d041a980154210c5d53f17c1be
Chris Hodapp <hodapp87@gmail.com>
2017-11-30 17:36:22 -0500

    Add requested plot; transfer code into Python files

--- graph_construction.py
+++ graph_construction.py
@@ -6,6 +6,83 @@
 # Date: 2017-11-28
 ###########################################################################
 
+import tensorflow as tf
+
+import keras
+import keras.backend as K
+from keras.layers import Dropout, Flatten, Input, merge, Dense, Activation
+from keras.layers.core import Lambda
+from keras.layers.convolutional import Conv2D, MaxPooling2D, ZeroPadding2D
+from keras.models import Model
+from keras.objectives import categorical_crossentropy
+from keras.optimizers import SGD
+
+def lrn(alpha=1e-4, k=2, beta=0.75, n=5, **kwargs):
+    """Returns a Keras layer implementing Local Response Normalization as
+    described in the AlexNet paper.
+    """
+    # Adapted from:
+    # https://github.com/heuritech/convnets-keras/blob/master/convnetskeras/customlayers.py#L9
+    def f(X):
+        b, r, c, ch = X.shape
+        half = n // 2
+        square = keras.backend.square(X)
+        extra_channels = K.spatial_2d_padding(K.permute_dimensions(square, (0, 2, 3, 1)), ((0, 0), (half, half)))
+        extra_channels = K.permute_dimensions(extra_channels, (0, 3, 1, 2))
+        scale = k
+        for i in range(n):
+            scale += alpha * extra_channels[:, :, :, i:i + int(ch)]
+        scale = scale ** beta
+        return X / scale
+
+    return Lambda(f, output_shape=lambda input_shape: input_shape, **kwargs)
+
+def get_keras_model(input_shape=(32, 32, 1), output_count=10, use_dropout=True):
+    """Returns a Keras model for the image classification on this dataset.
+    This is based heavily on AlexNet, but removes one of the initial
+    deep layers due to the smaller image sizes here.
+
+    Parameters:
+    input_shape -- Tuple with the model's input dimensions - default (32,32,1)
+    output_count -- Integer for number of output classes - default 10
+    use_dropout -- Boolean for whether to use dropout in training (default True)
+    """
+    inputs = Input(shape=input_shape)
+
+    # Convolutional layers:
+    
+    #conv_1 = Conv2D(96, kernel_size=(11,11), strides=(4, 4), activation='relu', name='conv_1')(inputs)
+    #conv_2 = MaxPooling2D((3, 3), strides=(2, 2))(conv_1)
+    #conv_2 = lrn(name='convpool_1')(conv_2)
+    #conv_2 = ZeroPadding2D((2, 2))(conv_2)
+    #conv_2 = Conv2D(256, kernel_size=(5,5), activation='relu', name='conv_2')(conv_2)
+    
+    # Names are retained to be clear how original AlexNet translates:
+    conv_2 = Conv2D(256, kernel_size=(5,5), activation='relu', name='conv_2')(inputs)
+    conv_3 = MaxPooling2D((3, 3), strides=(2, 2))(conv_2)
+    conv_3 = lrn()(conv_3)
+    conv_3 = ZeroPadding2D((1, 1))(conv_3)
+    conv_3 = Conv2D(384, kernel_size=(3, 3), activation='relu', name='conv_3')(conv_3)
+    conv_4 = ZeroPadding2D((1, 1))(conv_3)
+    conv_4 = Conv2D(384, kernel_size=(3, 3), activation='relu', name='conv_4')(conv_4)
+    conv_5 = ZeroPadding2D((1, 1))(conv_4)
+    conv_5 = Conv2D(128, kernel_size=(3, 3), activation='relu', name='conv_5')(conv_5)
+
+    # Fully-connected layers:
+    dense_1 = MaxPooling2D((3, 3), strides=(2, 2), name='convpool_5')(conv_5)
+    dense_1 = Flatten(name='flatten')(dense_1)
+    dense_1 = Dense(4096, activation='relu', name='dense_1')(dense_1)
+    if use_dropout:
+        dense_2 = Dropout(0.5)(dense_1)
+    dense_2 = Dense(4096, activation='relu', name='dense_2')(dense_2)
+    if use_dropout:
+        dense_3 = Dropout(0.5)(dense_2)
+    dense_3 = Dense(output_count, name='dense_3')(dense_3)
+    prediction = Activation('softmax', name='softmax')(dense_3)
+    
+    model = Model(inputs=[inputs], outputs=[prediction])
+    return model
+
 def classifier(learning_rate, use_dropout):
     """Builds (but does not execute) a TensorFlow graph for image
     classification on this dataset.
@@ -21,4 +98,19 @@ def classifier(learning_rate, use_dropout):
     x -- Node for input placeholder variable
     y -- Node for label placeholder variable
     """
-    pass
+    # I've started from a Keras model, rather than build a TensorFlow
+    # one, but in order to get certain underlying TensorFlow nodes I
+    # must compile it first, even if I don't use the Keras optimizer:
+    keras_model = get_keras_model(use_dropout=use_dropout)
+    model = tf.global_variables_initializer()
+    sgd = SGD(lr=0.02, decay=1e-6, momentum=0.9, nesterov=True)
+    keras_model.compile(loss='categorical_crossentropy',
+                        optimizer=sgd,
+                        metrics=['accuracy'])
+    accuracy = keras_model.metrics_tensors[0]
+    x = keras_model.inputs[0]
+    y = keras_model.targets[0]
+    predict = keras_model.outputs[0]
+    loss = tf.reduce_mean(categorical_crossentropy(y, predict))
+    train_op = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+    return (model, train_op, accuracy, x, y)
--- learn.py
+++ learn.py
@@ -6,6 +6,93 @@
 # Date: 2017-11-28
 ###########################################################################
 
-def train_model(model, num_epochs, batch_size, learning_rate):
-    pass
+import data_preprocessing
+import graph_construction
 
+import keras
+from keras.optimizers import SGD
+import numpy as np
+import matplotlib.pyplot as plt
+
+class AccuracyHistory(keras.callbacks.Callback):
+    """Keras callback for recording validation accuracy at regular
+    intervals (i.e. every set number of batches)
+    """
+    def __init__(self, test_X, test_y, skip=100):
+        """Initialize an AccuracyHistory.  After training, the 'accuracy'
+        property will contain a list of:
+        (batch num, validation accuracy, training accuracy)
+        
+        Parameters:
+        skip -- Interval (of batch number) at which to record accuracy
+        test_X -- Input data for predicting accuracy
+        test_y -- Correct labels corresponding to test_X
+        """
+        self.skip = skip
+        self.accuracy = []
+        self.test_X = test_X
+        self.test_y = test_y
+        self.i = 0
+    def on_batch_end(self, batch, logs={}):
+        self.i += 1
+        if self.i % self.skip == 0:
+            loss, val_acc = self.model.evaluate(self.test_X, self.test_y, verbose=0)
+            self.accuracy.append((self.i, val_acc, logs.get("acc")))
+
+def train_model(num_epochs, batch_size, learning_rate):
+    """Trains a neural network for image classification from the SVHN
+    dataset, and creates a plot giving training/testing accuracy as a
+    function of batch number.
+
+    Parameters:
+    num_epochs -- Number of training epochs
+    batch_size -- Number of examples in each training batch
+    learning_rate -- Initial learning rate
+    """
+    # Get data:
+    train_X_orig, train_y_orig, _, _ = data_preprocessing.load_data()
+    train_X_norm = data_preprocessing.normalize(train_X_orig)
+    train_X, valid_X, train_y, valid_y = data_preprocessing.split(
+        train_X_norm, train_y_orig)
+    # One-hot encode so they can be used for input/validation:
+    train_y_cat = keras.utils.to_categorical(train_y, num_classes=10)
+    valid_y_cat = keras.utils.to_categorical(valid_y, num_classes=10)
+    
+    # Build & compile model:
+    model = graph_construction.get_keras_model()
+    sgd = SGD(lr=learning_rate, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=sgd,
+                  metrics=['accuracy'])
+    # Add an extra dimension to fit with model's input format:
+    train_X2 = np.expand_dims(train_X, axis=3)
+    valid_X2 = np.expand_dims(valid_X, axis=3)
+
+    # Train:
+    acc_history = AccuracyHistory(valid_X2, valid_y_cat)    
+    history = model.fit(train_X2,
+                        train_y_cat,
+                        epochs=num_epochs,
+                        batch_size=batch_size,
+                        callbacks=[acc_history],
+                        validation_data=(valid_X2, valid_y_cat))
+    # The callback slows things down a bit, but I'm not sure of a good
+    # way around it.  If I were testing only on specific batches of
+    # validation data, it might be less of an issue.
+
+    fname_base = "model_{0}_{1}_{2}".format(learning_rate, num_epochs,
+                                            batch_size)
+    model.save_weights("{0}.h5".format(fname_base))
+
+    # Plot training/validation curve:
+    b = [i[0] for i in acc_history.accuracy]
+    plt.plot(b, [i[2] for i in acc_history.accuracy])
+    plt.plot(b, [i[1] for i in acc_history.accuracy])
+    plt.ylabel('Accuracy')
+    plt.xlabel('Batch')
+    plt.legend(['Training', 'Validation'], loc='lower right')
+    plt.savefig("{0}.png".format(fname_base))
+    plt.show()
+
+if __name__ == '__main__':
+    train_model(5, 64, 0.02)
